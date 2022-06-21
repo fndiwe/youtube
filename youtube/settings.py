80956b38e9a216fcfb6045daa850eb671e2077cf
Frankleen <93648270+Franklin-hub-cell@users.noreply.github.com>
2022-06-21 09:25:10 +0100

    fixed issue3

--- youtube/settings.py
+++ youtube/settings.py
@@ -127,7 +127,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
